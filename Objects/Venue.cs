b730efaaf167bf2b5f9876899ad05ff54821f274
Renee <meiqianye@gmail.com>
2017-03-03 09:04:46 -0800

    add test for get empty venues table at first; add getall method and deleteall method to venue class; pass the test

--- Objects/Venue.cs
+++ Objects/Venue.cs
@@ -8,7 +8,7 @@ namespace BandTracker
   {
     private int _id;
     private string _name;
-    
+
     public Venue(string Name, int Id = 0)
     {
       _id = Id;
@@ -23,6 +23,44 @@ namespace BandTracker
     public string GetName()
     {
       return _name;
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM venues;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+
+    public static List<Venue> GetAll()
+    {
+      List<Venue> allVenues = new List<Venue>{};
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        Venue newVenue = new Venue(name, id);
+        allVenues.Add(newVenue);
+      }
+
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allVenues;
     }
   }
 
--- Tests/BandTest.cs
+++ Tests/BandTest.cs
@@ -5,24 +5,11 @@ using Xunit;
 
 namespace BandTracker
 {
-  public class VenueTest : IDisposable
+  public class BandTest
   {
-    public VenueTest()
+    public BandTest()
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
-    }
-
-    public void Dispose()
-    {
-      Venue.DeleteAll();
-    }
-
-    [Fact]
-    public void GetAll_DatabaseEmptyAtFirst_Empty()
-    {
-      int result = Venue.GetAll().Count;
-
-      Assert.Equal(0, result);
     }
   }
 }
