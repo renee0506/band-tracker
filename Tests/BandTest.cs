cde477d2314e2be5e88db4cad035fed8721f2031
Renee <meiqianye@gmail.com>
2017-03-03 10:04:01 -0800

    add tests for save method

--- Tests/BandTest.cs
+++ Tests/BandTest.cs
@@ -35,6 +35,31 @@ namespace BandTracker
       Assert.Equal(band1, band2);
     }
 
+    [Fact]
+    public void Save_SaveToDatabase_Save()
+    {
+      Band testBand = new Band("THE SPACE");
+      testBand.Save();
+
+      List<Band> result = Band.GetAll();
+      List<Band> expected = new List<Band>{testBand};
+
+      Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Save_SaveToDatabase_SaveWithId()
+    {
+      Band testBand = new Band("THE SPACE");
+      testBand.Save();
+      Band savedBand = Band.GetAll()[0];
+
+      int result = savedBand.GetId();
+      int expected = testBand.GetId();
+
+      Assert.Equal(expected, result);
+    }
+
 
   }
 }
